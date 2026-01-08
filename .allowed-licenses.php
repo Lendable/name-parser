0ced3667030d6c30ec97cee8058879df6017edc8
Marcin Michalski <evulmastah@gmail.com>
2026-01-08 23:45:26 +0100

    ci: add licence checker

---
+++ .allowed-licenses.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+use Lendable\ComposerLicenseChecker\LicenseConfigurationBuilder;
+
+return (new LicenseConfigurationBuilder())
+    ->addLicenses(
+        'Apache-2.0',
+        'BSD-2-Clause',
+        'BSD-3-Clause',
+        'MIT',
+        'WTFPL'
+    )
+    ->build();
